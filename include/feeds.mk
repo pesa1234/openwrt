7aefaabfc6297ff82133a65ce1fdc6087db87f6f
Andrea Pesaresi <andreapesaresi82@gmail.com>
2024-11-19 21:55:46 +0100

    feeds.mk: adjust link to leave only specific to my repo
    Signed-off-by: Andrea Pesaresi <andreapesaresi82@gmail.com>

--- include/feeds.mk
+++ include/feeds.mk
@@ -5,6 +5,7 @@
 
 -include $(TMP_DIR)/.packageauxvars
 
+OWRT_LINK:=https://downloads.openwrt.org/snapshots/packages
 FEEDS_INSTALLED:=$(notdir $(wildcard $(TOPDIR)/package/feeds/*))
 FEEDS_AVAILABLE:=$(sort $(FEEDS_INSTALLED) $(shell $(SCRIPT_DIR)/feeds list -n 2>/dev/null))
 
@@ -36,12 +37,12 @@ define FeedSourcesAppendOPKG
 ( \
   echo 'src/gz %d_core %U/$(DATE)_$(VERSION_CODE)_$(BRANCH)/targets/%S/packages'; \
   $(strip $(if $(CONFIG_PER_FEED_REPO), \
-	echo 'src/gz %d_base %U/packages/%A/base'; \
+	echo 'src/gz %d_base $(OWRT_LINK)/packages/%A/base'; \
 	$(if $(filter %SNAPSHOT-y,$(VERSION_NUMBER)-$(CONFIG_BUILDBOT)), \
 		echo 'src/gz %d_kmods %U/$(DATE)_$(VERSION_CODE)_$(BRANCH)/targets/%S/kmods/$(LINUX_VERSION)-$(LINUX_RELEASE)-$(LINUX_VERMAGIC)';) \
 	$(foreach feed,$(FEEDS_AVAILABLE), \
 		$(if $(CONFIG_FEED_$(feed)), \
-			echo '$(if $(filter m,$(CONFIG_FEED_$(feed))),# )src/gz %d_$(feed) %U/packages/%A/$(feed)';)))) \
+			echo '$(if $(filter m,$(CONFIG_FEED_$(feed))),# )src/gz %d_$(feed) $(OWRT_LINK)/packages/%A/$(feed)';)))) \
 ) >> $(1)
 endef
 
@@ -50,12 +51,12 @@ define FeedSourcesAppendAPK
 ( \
   echo '%U/$(DATE)_$(VERSION_CODE)_$(BRANCH)/targets/%S/packages/packages.adb'; \
   $(strip $(if $(CONFIG_PER_FEED_REPO), \
-	echo '%U/packages/%A/base/packages.adb'; \
+	echo '$(OWRT_LINK)/%A/base/packages.adb'; \
 	$(if $(filter %SNAPSHOT-y,$(VERSION_NUMBER)-$(CONFIG_BUILDBOT)), \
 		echo '%U/$(DATE)_$(VERSION_CODE)_$(BRANCH)/targets/%S/kmods/$(LINUX_VERSION)-$(LINUX_RELEASE)-$(LINUX_VERMAGIC)/packages.adb';) \
 	$(foreach feed,$(FEEDS_AVAILABLE), \
 		$(if $(CONFIG_FEED_$(feed)), \
-			echo '$(if $(filter m,$(CONFIG_FEED_$(feed))),# )%U/packages/%A/$(feed)/packages.adb';)))) \
+			echo '$(if $(filter m,$(CONFIG_FEED_$(feed))),# )$(OWRT_LINK)/%A/$(feed)/packages.adb';)))) \
 ) >> $(1)
 endef
 
